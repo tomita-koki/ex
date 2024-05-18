5ce8a6ec5917006f26acb1c4becdc90df9caac71
tomita-koki <pf9555afd752se05se@softbank.ne.jp>
2024-05-18 10:19:45 +0900

    ラベル、もっと見る機能　追加（機能表示しない、、）

--- src/js/main.js
+++ src/js/main.js
@@ -115,3 +115,52 @@ doc.querySelectorAll('.js-label-01').forEach((elm) => {
 });
 
 console.log('testgfds');
+
+// -----------------------------------------
+// もっと見るボタン
+// -----------------------------------------
+const viewMore = (root) => {
+    let flag = false;
+    const viewMore = root;
+    const viewMoreItem = viewMore.querySelectorAll('.js-viewMore-item');
+    const btn = viewMore.querySelector('.js-viewMore-btn');
+    const btnText = viewMore.querySelector('.js-viewMore-btn-text');
+    const initialVisibleItems = 5;
+
+    if(!viewMoreItem.length || !btn || !btnText) {
+        return;
+    } else if(viewMoreItem.length < initialVisibleItems) {
+        btn.style.display = "none";
+
+        return;
+    }
+
+    // 初期表示以外の要素を非表示にする
+    for (let i = initialVisibleItems; i < viewMoreItem.length; i++) {
+        viewMoreItem[i].classList.add("hidden");
+    }
+        
+    btn.addEventListener('click', () => {
+        if(flag) {
+            for (let i = initialVisibleItems; i < viewMoreItem.length; i++) {
+                viewMoreItem[i].classList.add("hidden");
+                flag = false;
+            }
+            btnText.textContent = "もっと見る";
+        } else {
+            for (let i = initialVisibleItems; i < viewMoreItem.length; i++) {
+                viewMoreItem[i].classList.remove("hidden");
+                flag =true;
+            }
+            btnText.textContent = "閉じる";
+        }
+        btn.classList.toggle('close');
+    });
+};
+
+(() => {
+    const roots = document.querySelectorAll(".js-viewMore");
+    for (let i = 0; i < roots.length; i++) {
+        viewMore(roots[i]);
+    }
+})();
