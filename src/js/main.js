bbc839ae1442f63ff47e0ddaf6fd3f7c642bfa06
tomita-koki <pf9555afd752se05se@softbank.ne.jp>
2024-05-21 21:36:34 +0900

    0520時点

--- src/js/main.js
+++ src/js/main.js
@@ -164,3 +164,10 @@ const viewMore = (root) => {
         viewMore(roots[i]);
     }
 })();
+
+const ham = document.querySelector('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
+const nav = document.querySelector('#js-nav'); //js-navの要素を取得し、変数navに格納
+
+ham.addEventListener('click', function () { //ハンバーガーメニューをクリックしたら
+  console.log('ok');
+  });
