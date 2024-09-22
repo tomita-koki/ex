c30490dce7d5bf555c58153439379a6d2aaa1e08
tomita-koki <pf9555afd752se05se@softbank.ne.jp>
2024-09-22 21:44:06 +0900

    ストップウォッチ 追加要素

--- src/js/main.js
+++ src/js/main.js
@@ -48,4 +48,104 @@ const viewMore = (root) => {
     }
 })();
 
-console.log("fdsaf");
+// -----------------------------------------
+// ストップウォッチ
+// -----------------------------------------
+
+const time = document.getElementById('time');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
+// 開始時間
+let startTime;
+// 停止時間
+let stopTime = 0;
+// タイムアウトID
+let timeoutID;
+
+// 時間を表示する関数
+function displayTime() {
+  const currentTime = new Date(Date.now() - startTime + stopTime);
+  const h = String(currentTime.getHours()-1).padStart(2, '0');
+  const m = String(currentTime.getMinutes()).padStart(2, '0');
+  const s = String(currentTime.getSeconds()).padStart(2, '0');
+  const ms = String(currentTime.getMilliseconds()).padStart(3, '0');
+
+  time.textContent = `${h}:${m}:${s}.${ms}`;
+  timeoutID = setTimeout(displayTime, 10);
+}
+
+// スタートボタンがクリックされたら時間を進める
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  resetButton.disabled = true;
+  startTime = Date.now();
+  displayTime();
+});
+
+// ストップボタンがクリックされたら時間を止める
+stopButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = false;
+  clearTimeout(timeoutID);
+  stopTime += (Date.now() - startTime);
+});
+
+// リセットボタンがクリックされたら時間を0に戻す
+resetButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = true;
+  time.textContent = '00:00:00.000';
+  stopTime = 0;
+});
+
+// const price = document.getElementById('price');
+
+// console.log(price);
+
+// price.textContent = i;
+
+// i + i * 0.1
+
+// function() {
+//     i + i * 0.1
+// }
+
+function total(i) {
+    const tax = 0.1;
+    return i + i * tax;
+}
+
+document.getElementById('price').textContent = total(800);
+
+
+
+//DOM
+const button =document.getElementById("addBtn");
+const lists =document.getElementById("lists");
+
+//関数(メゾット)
+async function getUsers() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    return users;
+}
+
+function addList(user) {
+    const list =document.createElement("li");
+    list.innerText = user.name;
+    lists.appendChild(list);
+}
+
+async function listUers() {
+    const users = await getUsers();
+    users.forEach(addList);
+}
+
+//イベント
+window.addEventListener("load", listUers);
+button.addEventListener("click", listUers);
