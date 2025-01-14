aa19ee9b912b05c30d45e5633d476259e14e517a
tomita-koki <pf9555afd752se05se@softbank.ne.jp>
2025-01-14 20:20:39 +0900

    ブラウザシンク＆gulp 完成！

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,29 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
 
 // Sassのコンパイルタスク
 gulp.task('cssSass', function () {
-    return gulp.src('_dev/scss/**/*.scss')  // すべてのSCSSファイルを対象に
+    return gulp.src('_dev/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('src/css'));
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());  // 変更後にブラウザをリロードせずスタイルのみ反映
 });
 
-// ウォッチタスク（変更を監視してcssSassタスクを実行）
+// ブラウザシンクの初期化タスク
+gulp.task("browserSyncTask", function () {
+    browserSync.init({
+        server: {
+            baseDir: "src",
+        },
+    });
+});
+
+// ウォッチタスクの設定
 gulp.task('watch', function () {
     gulp.watch('_dev/scss/**/*.scss', gulp.series('cssSass'));  
+    gulp.watch("src/**").on('change', browserSync.reload); // 変更があればリロード
 });
 
-// デフォルトタスクでwatchを実行
-gulp.task('default', gulp.series('cssSass', 'watch'));
-
-
+// デフォルトタスク
+gulp.task('default', gulp.parallel('cssSass', 'browserSyncTask', 'watch'));
