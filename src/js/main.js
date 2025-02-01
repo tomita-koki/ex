b6e4872c16e6c81d55ca4216505737c7c53251b2
tomita-koki <pf9555afd752se05se@softbank.ne.jp>
2025-02-01 18:21:56 +0900

    0201

--- src/js/main.js
+++ src/js/main.js
@@ -3,50 +3,50 @@
 // -----------------------------------------
 // もっと見るボタン
 // -----------------------------------------
-const viewMore = (root) => {
-    let flag = false;
-    const viewMore = root;
-    const viewMoreItem = viewMore.querySelectorAll('.js-viewMore-item');
-    const btn = viewMore.querySelector('.js-viewMore-btn');
-    const btnText = viewMore.querySelector('.js-viewMore-btn-text');
-    const initialVisibleItems = 5;
+// const viewMore = (root) => {
+//     let flag = false;
+//     const viewMore = root;
+//     const viewMoreItem = viewMore.querySelectorAll('.js-viewMore-item');
+//     const btn = viewMore.querySelector('.js-viewMore-btn');
+//     const btnText = viewMore.querySelector('.js-viewMore-btn-text');
+//     const initialVisibleItems = 5;
 
-    if (!viewMoreItem.length || !btn || !btnText) {
-        return;
-    } else if (viewMoreItem.length < initialVisibleItems) {
-        btn.style.display = "none";
-        return;
-    }
+//     if (!viewMoreItem.length || !btn || !btnText) {
+//         return;
+//     } else if (viewMoreItem.length < initialVisibleItems) {
+//         btn.style.display = "none";
+//         return;
+//     }
 
-    // 初期表示以外の要素を非表示にする
-    for (let i = initialVisibleItems; i < viewMoreItem.length; i++) {
-        viewMoreItem[i].classList.add("hidden");
-    }
+//     // 初期表示以外の要素を非表示にする
+//     for (let i = initialVisibleItems; i < viewMoreItem.length; i++) {
+//         viewMoreItem[i].classList.add("hidden");
+//     }
 
-    btn.addEventListener('click', () => {
-        if (flag) {
-            for (let i = initialVisibleItems; i < viewMoreItem.length; i++) {
-                viewMoreItem[i].classList.add("hidden");
-            }
-            flag = false;
-            btnText.textContent = "もっと見る";
-        } else {
-            for (let i = initialVisibleItems; i < viewMoreItem.length; i++) {
-                viewMoreItem[i].classList.remove("hidden");
-            }
-            flag = true;
-            btnText.textContent = "閉じる";
-        }
-        btn.classList.toggle('close');
-    });
-};
+//     btn.addEventListener('click', () => {
+//         if (flag) {
+//             for (let i = initialVisibleItems; i < viewMoreItem.length; i++) {
+//                 viewMoreItem[i].classList.add("hidden");
+//             }
+//             flag = false;
+//             btnText.textContent = "もっと見る";
+//         } else {
+//             for (let i = initialVisibleItems; i < viewMoreItem.length; i++) {
+//                 viewMoreItem[i].classList.remove("hidden");
+//             }
+//             flag = true;
+//             btnText.textContent = "閉じる";
+//         }
+//         btn.classList.toggle('close');
+//     });
+// };
 
-(() => {
-    const roots = document.querySelectorAll(".js-viewMore");
-    for (let i = 0; i < roots.length; i++) {
-        viewMore(roots[i]);
-    }
-})();
+// (() => {
+//     const roots = document.querySelectorAll(".js-viewMore");
+//     for (let i = 0; i < roots.length; i++) {
+//         viewMore(roots[i]);
+//     }
+// })();
 
 
 // -----------------------------------------
@@ -97,106 +97,27 @@ const viewMore = (root) => {
     }
 })();
 
-// -----------------------------------------
-// ストップウォッチ
-// -----------------------------------------
 
-const time = document.getElementById('time');
-const startButton = document.getElementById('start');
-const stopButton = document.getElementById('stop');
-const resetButton = document.getElementById('reset');
+const kokiHeader = document.querySelector('.kokiHeader');
+const nav = document.querySelector('.kokiHeader__nav');
+const hamburger = document.getElementById("hamburger");
+const btn = document.getElementById("hamburger__btn");
+const main = document.querySelector(".background");
+console.log(main);
 
-// 開始時間
-let startTime;
-// 停止時間
-let stopTime = 0;
-// タイムアウトID
-let timeoutID;
-
-// 時間を表示する関数
-function displayTime() {
-  const currentTime = new Date(Date.now() - startTime + stopTime);
-  const h = String(currentTime.getHours()-1).padStart(2, '0');
-  const m = String(currentTime.getMinutes()).padStart(2, '0');
-  const s = String(currentTime.getSeconds()).padStart(2, '0');
-  const ms = String(currentTime.getMilliseconds()).padStart(3, '0');
-
-  time.textContent = `${h}:${m}:${s}.${ms}`;
-  timeoutID = setTimeout(displayTime, 10);
-}
-
-// スタートボタンがクリックされたら時間を進める
-startButton.addEventListener('click', () => {
-  startButton.disabled = true;
-  stopButton.disabled = false;
-  resetButton.disabled = true;
-  startTime = Date.now();
-  displayTime();
+btn.addEventListener("click", () => {
+    const active = hamburger.classList.contains('active');
+    if(active) {
+        kokiHeader.classList.remove('active');
+        hamburger.classList.remove('active');
+        main.classList.remove('active');
+        const alertmsg = function(){
+            // nav.classList.remove('active');
+          }
+          setTimeout(alertmsg, 10);
+    } else {
+        kokiHeader.classList.add('active');
+        hamburger.classList.add('active');
+        main.classList.add('active');
+    }
 });
-
-// ストップボタンがクリックされたら時間を止める
-stopButton.addEventListener('click', function() {
-  startButton.disabled = false;
-  stopButton.disabled = true;
-  resetButton.disabled = false;
-  clearTimeout(timeoutID);
-  stopTime += (Date.now() - startTime);
-});
-
-// リセットボタンがクリックされたら時間を0に戻す
-resetButton.addEventListener('click', function() {
-  startButton.disabled = false;
-  stopButton.disabled = true;
-  resetButton.disabled = true;
-  time.textContent = '00:00:00.000';
-  stopTime = 0;
-});
-
-// const price = document.getElementById('price');
-
-// console.log(price);
-
-// price.textContent = i;
-
-// i + i * 0.1
-
-// function() {
-//     i + i * 0.1
-// }
-
-function total(i) {
-    const tax = 0.1;
-    return i + i * tax;
-}
-
-document.getElementById('price').textContent = total(800);
-
-
-
-//DOM
-const button =document.getElementById("addBtn");
-const lists =document.getElementById("lists");
-
-//関数(メゾット)
-async function getUsers() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
-    return users;
-}
-
-function addList(user) {
-    const list =document.createElement("li");
-    list.innerText = user.name;
-    lists.appendChild(list);
-}
-
-async function listUers() {
-    const users = await getUsers();
-    users.forEach(addList);
-}
-
-//イベント
-window.addEventListener("load", listUers);
-button.addEventListener("click", listUers);
-
-
